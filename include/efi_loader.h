48b164efd0aa45fe6afff7c29062e445404487ad
Alexander Graf <agraf@suse.de>
2015-12-22 12:12:42 +0000

    efi_loader: Add runtime services
    After booting has finished, EFI allows firmware to still interact with the OS
    using the "runtime services". These callbacks live in a separate address space,
    since they are available long after U-Boot has been overwritten by the OS.
    
    This patch adds enough framework for arbitrary code inside of U-Boot to become
    a runtime service with the right section attributes set. For now, we don't make
    use of it yet though.
    
    We could maybe in the future map U-boot environment variables to EFI variables
    here.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    
    ---
    
    v1 -> v2:
    
      - Fix runtime service sections
      - Add runtime detach
      - Enable runtime relocations
      - Add get_time
      - Fix relocation
      - Fix 32bit
      - Add am335x support
      - Move section definition to header
      - Add systab to runtime section
      - Add self-relocation hook table
      - Fix self-relocation
      - Relocate efi_runtime section early during bootup
      - Fix return values for a number of callbacks to be more UEFI compliant
      - Move to GPLv2+

--- include/efi_loader.h
+++ include/efi_loader.h
@@ -29,6 +29,7 @@
 
 #define EFI_EXIT(ret) efi_exit_func(ret);
 
+extern struct efi_runtime_services efi_runtime_services;
 extern struct efi_system_table systab;
 
 extern const struct efi_simple_text_output_protocol efi_con_out;
@@ -38,6 +39,9 @@ extern const struct efi_console_control_protocol efi_console_control;
 extern const efi_guid_t efi_guid_console_control;
 extern const efi_guid_t efi_guid_device_path;
 extern const efi_guid_t efi_guid_loaded_image;
+
+extern unsigned int __efi_runtime_start, __efi_runtime_stop;
+extern unsigned int __efi_runtime_rel_start, __efi_runtime_rel_stop;
 
 struct efi_class_map {
 	const efi_guid_t *guid;
@@ -68,12 +72,18 @@ void *efi_load_pe(void *efi, struct efi_loaded_image *loaded_image_info);
 void efi_save_gd(void);
 void efi_restore_gd(void);
 efi_status_t efi_exit_func(efi_status_t ret);
+void efi_runtime_relocate(ulong offset, struct efi_mem_desc *map);
 
 #define EFI_LOADER_POOL_SIZE (128 * 1024 * 1024)
 void *efi_loader_alloc(uint64_t len);
 
+#define EFI_RUNTIME_DATA __attribute__ ((section ("efi_runtime_data")))
+#define EFI_RUNTIME_TEXT __attribute__ ((section ("efi_runtime_text")))
+
 #else /* defined(EFI_LOADER) */
 
+#define EFI_RUNTIME_DATA
+#define EFI_RUNTIME_TEXT
 static inline void efi_restore_gd(void) { }
 
 #endif
--- lib/efi_loader/efi_boottime.c
+++ lib/efi_loader/efi_boottime.c
@@ -36,7 +36,7 @@ static bool efi_is_direct_boot = true;
  * In most cases we want to pass an FDT to the payload, so reserve one slot of
  * config table space for it. The pointer gets populated by do_bootefi_exec().
  */
-static struct efi_configuration_table efi_conf_table[] = {
+static struct efi_configuration_table EFI_RUNTIME_DATA efi_conf_table[] = {
 	{
 		.guid = EFI_FDT_GUID,
 	},
@@ -741,10 +741,10 @@ static const struct efi_boot_services efi_boot_services = {
 };
 
 
-static uint16_t firmware_vendor[] =
+static uint16_t EFI_RUNTIME_DATA firmware_vendor[] =
 	{ 'D','a','s',' ','U','-','b','o','o','t',0 };
 
-struct efi_system_table systab = {
+struct efi_system_table EFI_RUNTIME_DATA systab = {
 	.hdr = {
 		.signature = EFI_SYSTEM_TABLE_SIGNATURE,
 		.revision = 0x20005, /* 2.5 */
