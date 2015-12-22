3e6e6b52f9cdca024199e65f4e4f5840a61e1b64
Alexander Graf <agraf@suse.de>
2015-12-22 12:04:11 +0000

    efi_loader: Add boot time services
    When an EFI application runs, it has access to a few descriptor and callback
    tables to instruct the EFI compliant firmware to do things for it. The bulk
    of those interfaces are "boot time services". They handle all object management,
    and memory allocation.
    
    This patch adds support for the boot time services and also exposes a system
    table, which is the point of entry descriptor table for EFI payloads.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- include/efi_loader.h
+++ include/efi_loader.h
@@ -24,14 +24,55 @@
 #include <efi_api.h>
 #include <linux/list.h>
 
+/* #define DEBUG_EFI */
+
+#ifdef DEBUG_EFI
+#define EFI_ENTRY(format, ...) do { \
+	efi_restore_gd(); \
+	printf("EFI: Entry %s(" format ")\n", __func__, ##__VA_ARGS__); \
+	} while(0)
+#else
+#define EFI_ENTRY(format, ...) do { \
+	efi_restore_gd(); \
+	} while(0)
+#endif
+
+#define EFI_EXIT(ret) efi_exit_func(ret);
+
+extern struct efi_system_table systab;
+
 extern const efi_guid_t efi_guid_device_path;
 extern const efi_guid_t efi_guid_loaded_image;
+
+struct efi_class_map {
+	const efi_guid_t *guid;
+	const void *interface;
+};
+
+struct efi_handler {
+	const efi_guid_t *guid;
+	efi_status_t (EFIAPI *open)(void *handle,
+			efi_guid_t *protocol, void **protocol_interface,
+			void *agent_handle, void *controller_handle,
+			uint32_t attributes);
+};
+
+struct efi_object {
+	struct list_head link;
+	struct efi_handler protocols[4];
+	void *handle;
+};
+extern struct list_head efi_obj_list;
 
 efi_status_t efi_return_handle(void *handle,
 		efi_guid_t *protocol, void **protocol_interface,
 		void *agent_handle, void *controller_handle,
 		uint32_t attributes);
+void efi_timer_check(void);
 void *efi_load_pe(void *efi, struct efi_loaded_image *loaded_image_info);
+void efi_save_gd(void);
+void efi_restore_gd(void);
+efi_status_t efi_exit_func(efi_status_t ret);
 
 #define EFI_LOADER_POOL_SIZE (128 * 1024 * 1024)
 void *efi_loader_alloc(uint64_t len);
